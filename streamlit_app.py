efd4ca430017ecc3c66225a768a15d733d070d9a
shi1720 <162955781+shi1720@users.noreply.github.com>
2024-11-22 12:38:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+# Add the 'src' directory to the Python path
+sys.path.append(str(Path(__file__).resolve().parent / "src"))
+
 from streamlit_db_demo.app import main
 
 if __name__ == "__main__":
